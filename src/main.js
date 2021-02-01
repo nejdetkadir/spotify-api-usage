c6865f6cb587e0f0483b8e07201fae7ace5135ba
nejdetkadir <nejdetkadir.550@gmail.com>
2021-02-01 06:30:00 +0300

    Install vue-axios

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 new Vue({
