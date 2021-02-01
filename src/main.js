35e0b51aac66991fd5acb16d848cdb612b1db347
nejdetkadir <nejdetkadir.550@gmail.com>
2021-02-01 06:31:38 +0300

    Install vuex

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import store from './store'
 
 Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  store,
+  render: h => h(App)
 }).$mount('#app')
