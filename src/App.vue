78bcf79b699fad569c5d5f3dc90b1cad6cb9591a
nejdetkadir <nejdetkadir.550@gmail.com>
2021-02-01 06:47:29 +0300

    Add pug support

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,14 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  .container.text-center
+    img(alt='Vue logo' src='./assets/logo.png')
+    p spotify api usage
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
